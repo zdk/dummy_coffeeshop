cac6fee0bc69759a98130e83882309d4f4051803
zdk <di.warachet@gmail.com>
2024-06-18 12:09:50 +0700

    Use gin

--- main.go
+++ main.go
@@ -1,23 +1,48 @@
 package main
 
 import (
-	"cofeeshop/coffee"
-	"fmt"
+	"coffeeshop/coffee"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// fmt.Println("Printing the lis of coffees available in the Coffee Shop")
-	coffees, err := coffee.GetCoffees()
-	if err != nil {
-		fmt.Println("Error while getting coffeelist ", err)
-		return
-	}
-
-	for _, element := range coffees.List {
-		result := fmt.Sprintf("%s for $%v", element.Name, element.Price)
-		fmt.Println(result)
-	}
-
-	fmt.Println("Is Latte Available? ", coffee.IsCoffeeAvailable("Latte"))
-	fmt.Println("Is Americano Available? ", coffee.IsCoffeeAvailable("Americano"))
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to the Coffeeshop!",
+		})
+	})
+	r.GET("/coffee", getCoffee)
+	r.Run(":8081")
 }
+
+func getCoffee(c *gin.Context) {
+	coffeelist, _ := coffee.GetCoffees()
+	c.String(http.StatusOK, " %s", coffeelist)
+}
+
+// package main
+//
+// import (
+// 	"cofeeshop/coffee"
+// 	"fmt"
+// )
+//
+// func main() {
+// 	// fmt.Println("Printing the lis of coffees available in the Coffee Shop")
+// 	coffees, err := coffee.GetCoffees()
+// 	if err != nil {
+// 		fmt.Println("Error while getting coffeelist ", err)
+// 		return
+// 	}
+//
+// 	for _, element := range coffees.List {
+// 		result := fmt.Sprintf("%s for $%v", element.Name, element.Price)
+// 		fmt.Println(result)
+// 	}
+//
+// 	fmt.Println("Is Latte Available? ", coffee.IsCoffeeAvailable("Latte"))
+// 	fmt.Println("Is Americano Available? ", coffee.IsCoffeeAvailable("Americano"))
+// }
